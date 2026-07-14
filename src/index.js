77487a1d07d122e448959261421c3180d26f62f0
MockForMe Developer <developer@mockforme.com>
2026-07-14 17:22:58 +0530

    chore: always pick the laltest module from jsdelivr

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { mockforme } from 'mockforme';
+// eslint-disable-next-line no-undef
 mockforme().run();
 
 
